7b2255b1a7e3eb44d4a4cf0a98fd78bb0749115b
Vanilson66 <vanilsonreact@gmail.com>
2024-05-18 16:25:10 -0300

    homepage created

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,16 @@ import styles from "./index.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Employee Manager</h1>
+      <h1 className={styles.title}>Dashboard Administrativo</h1>
+
+      <div className={styles.container}>
+        <div className={styles.toolsbar}>
+          <button>Adicionar funcionário</button>
+        </div>
+
+        <div>
+        </div>
+      </div>
     </main>
   );
 }
