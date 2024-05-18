171fa27716bc82086bd71e0a2b2bd4f6fc49137f
Vanilson66 <vanilsonreact@gmail.com>
2024-05-18 16:08:49 -0300

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Employee Manager</h1>
+    </main>
+  );
+}
