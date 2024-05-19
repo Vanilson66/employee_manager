801673be2a53366b27d1bdedb89f773f9b7c31bd
Vanilson66 <vanilsonreact@gmail.com>
2024-05-18 23:31:58 -0300

    node+mongo initial setup complete

---
+++ backend/src/confg/express.ts
@@ -0,0 +1,7 @@
+export const expressConfig = {
+  port: process.env.PORT || 4000
+}
+
+export const mongoConfig = {
+  url: process.env.MONGO_URL || 'mongodb://default-url'
+}
---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import mongoose from "mongoose";
+import { expressConfig, mongoConfig } from "./confg/express";
+
+const app = express()
+
+app.use(cors({
+  credentials: true
+}))
+
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(expressConfig.port, () => {
+  console.log(`Server running on "http://localhost:${expressConfig.port}"`)
+})
+
+mongoose.Promise = Promise
+mongoose.connect(mongoConfig.url)
+mongoose.connection.on("error", (error: Error) => console.log(error))
